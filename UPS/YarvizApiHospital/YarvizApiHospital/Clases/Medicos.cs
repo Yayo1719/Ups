c8a0af90c5eed7231d12593f674a928b19f06fe7
Yayo <yayo1719@hotmail.com>
2023-08-15 00:08:16 -0500

    api

---
+++ UPS/YarvizApiHospital/YarvizApiHospital/Clases/Medicos.cs
@@ -0,0 +1,19 @@
+﻿namespace YarvizApiHospital.Clases
+{
+    public class Medico
+    {
+        public int Codigo { get; set; }
+        public string TipoIdentificacion { get; set; }
+        public string Identificaion { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string Telefono { get; set; }
+        public string FechaNacimiento { get; set; }
+        public string EstadoUSuario { get; set; }
+        public string EstadoMedico { get; set; }
+        public string Especialidad { get; set; }
+
+    }
+}
---
+++ UPS/YarvizApiHospital/YarvizApiHospital/Controllers/HorpitalControllers.cs
@@ -0,0 +1,144 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using YarvizApiHospital.Clases;
+
+namespace YarvizApiHospital.Controllers
+{
+    [ApiController]
+    [Route("Medicos")]
+
+    public class Medicos : Controller
+    {
+        [HttpGet]
+        [Route("Listar")]
+
+        public dynamic Listar()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+
+            //Medico Medico1 = new Medico
+
+
+            //{
+            //    Codigo = 1,
+            //    TipoIdentificacion = "1"
+
+            //};
+
+
+            //return 1;
+        }
+
+        [HttpPost]
+        [Route("Nuevo")]
+
+        public dynamic GuardarMedico()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+        }
+    }
+
+    [ApiController]
+    [Route("Pacientes")]
+
+    public class Pacientes : Controller
+    {
+        [HttpGet]
+        [Route("Listar")]
+
+        public dynamic Listar()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+        }
+
+        [HttpGet]
+        [Route("Nuevo")]
+
+        public dynamic GuardarMedico()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+        }
+    }
+
+    [ApiController]
+    [Route("Citas")]
+
+    public class Citas : Controller
+    {
+        [HttpGet]
+        [Route("Listar")]
+
+        public dynamic Listar()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+        }
+
+        [HttpGet]
+        [Route("Nuevo")]
+
+        public dynamic GuardarMedico()
+        {
+            List<Medico> list = new List<Medico>
+            {
+                new Medico
+                {
+                    Codigo = 1,
+                    TipoIdentificacion = "1"
+
+                }
+            };
+
+            return list;
+        }
+    }
+}
